5fa2ca63ffd8d71119cdab70c9e511d15adaaae5
cjnes <c.nesbit@sinclair.edu>
2022-11-29 09:57:07 -0500

    Created overloaded half function and added data for testing

--- Templates/Templates.cpp
+++ Templates/Templates.cpp
@@ -7,7 +7,18 @@ T half(T num)
 	return num / 2.0;
 }
 
+int half(int num)
+{
+	return round(static_cast<float>(num) / 2.0);
+}
+
 int main()
 {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
 
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
 }
